632e94bdee7bc3491c9eceffa0fc743f04e7fefb
vliegbanaan <56888959+vliegbanaan@users.noreply.github.com>
2023-04-05 13:44:23 +0200

    Create nws.py
    skelet opgezet.

---
+++ nws.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+def scan_host(ip_address):
+    """
+    Hier moet scan hosts komen
+    """
+    # TODO
+
+def scan_subnet(subnet):
+    """
+    Scan subnet
+    """
+    # TODO
+def detect_services(ip_address):
+    """
+   Hier moet detecteer services bij openstaande poort
+    """
+    # TODO
+
+def detect_os(ip_address):
+    """
+    Hier komt detecteer OS
+    """
+    # TODO
+
+def format_output(hosts):
+    """
+    Print resultaten in een mooie F string.
+    """
+    # TODO
+
+def main():
+    """
+    main functie die de respectievelijke functies aanroept.
+    """
+    # TODO
+
+if __name__ == '__main__':
+    main()
