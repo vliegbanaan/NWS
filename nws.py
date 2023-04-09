2e78bea3503e3c8a2aa33ffaf5b3af68b0b8d6d0
vliegbanaan <56888959+vliegbanaan@users.noreply.github.com>
2023-04-09 16:12:30 +0200

    Update nws.py
    docstring added

--- nws.py
+++ nws.py
@@ -8,29 +8,35 @@ def scan_host(ip_address):
     Scans a single host and returns a tuple containing the IP address, MAC address, open ports, hostname (if available),
     and operating system (if available)
     """
-    # getMac van de host
+    # Mac adres van het opgegeven IP adres
     mac_address = get_mac_address(ip_address)
 
-    # Scan voor openstaande poorten op de host.
+    # Scan voor openstaande poorten op opgegeven host
     open_ports = detect_open_ports(ip_address)
 
-    # Get de hostname van de host (als het beschikbaar is).
+    # Get hostname van de host van het opgegeven IP adres.
     hostname = get_hostname(ip_address)
 
-    # Scan voor de services op de poorten.
+    # Scan voor de services op de poorten van het opgegeven IP adres.
     services = detect_services(ip_address)
 
-    # Get the operating system of the host (if available)
+    # Get operating system van het apparaat van het opgegeven IP adres.
     os_name = detect_os(ip_address)
 
-    # Return the results as a formatted string
+    #  Retouneer het in een F string.
     return f'IP address: {ip_address}\nMAC address: {mac_address}\nOpen ports: {", ".join(str(port) for port in open_ports)} \nHostname: {hostname}\n Operating system: {os_name}, \n Detected services: {detect_services}' 
     
 # Scan host moet doorverwijzen naar de andere functies, en deze functies halen allemaal apart de waardes op en format ze terug als F string. alles returnen naar scan_host, en deze returned het naar main.
 
 def scan_subnet(ip_subnet):
     """
-    Scan subnet
+    Scan het opgegeven subnet voor apparaten voor een ARP request en retourneer deze in een lijst van dictionaries die de IP's en MAC addressen bevatten van de apparaten in het opgegeven subnet.
+
+    Arg:
+        ip_subnet (string): De sybbet wat gescanned moet worden in CIDR notatie, dat is bijvoorbeeld 192.168.1.1.
+
+    Return:
+        list: een lijst van dictionaries, elk die de IP en mac addressen bevatten van de appparaten op het subnet.
     """
     arp = ARP(pdst=ip_subnet)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -42,14 +48,17 @@ def scan_subnet(ip_subnet):
     for sent, received in result:
         clients.append({'ip': received.psrc, 'mac': received.hwsrc})
 
-    print("beschikbare devices: ")
+    print("beschikbare apparaten: ")
     print("IP" + " " * 18+"MAC")
     for client in clients:
         print("{:16}    {}".format(client['ip'], client['mac']))
 
 def get_mac_address(ip_address):
     """
-    Verkrijg MAC adres door ARP
+    Verkrijg de mac addressen van de apparaten in het opgegeven subnet d.m.v. een ARP-request."
+
+    Return:
+        Retouneer het MAC adres als deze gevonden is, anders een none.
     """
     arp = ARP(pdst=ip_address)
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")
@@ -59,7 +68,10 @@ def get_mac_address(ip_address):
 
 def is_port_open(ip_address, port):
     """
-    Check of de TCP poort open is op een host met de gegeven IP.
+    Controleer of de TCP poort open is op de host van het opgegeven IP adres.
+    
+    Return:
+        
     """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.settimeout(0.1)
@@ -107,19 +119,6 @@ def detect_os(ip_address):
     nm.scan(hosts=ip_address, arguments='-A')
     os_name = nm[ip_address]['osmatch'][0]['name']
     return os_name
-
-def detect_services(ip_address):
-    """
-   Hier moet detecteer services bij openstaande poort
-    """
-    # TODO
-    print("Detect Services")
-
-def format_output(hosts):
-    """
-    Print resultaten in een mooie F string.
-    """
-    # TODO
 
 def main():
     choice = input("Kies een optie: \n 1. Scan Host \n 2. Scan Subnet \n")
